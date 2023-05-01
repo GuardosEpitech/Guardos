1e77ad83847ebf48bfc0c705c4125fe9272346d2
Alban-de-tourtier <alban.de-tourtier@epitech.eu>
2023-05-01 17:34:20 +0200

    [ADD] add common compoents

---
+++ packages/restoWeb/src/pages/AddRestaurantPage/AddRestaurantPage.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Header from "@src/components/dumpComponents/Header/Header";
+import Layout from 'shared/components/Layout/Layout';
+import RestaurantForm
+  from "@src/components/forms/RestaurantForm/RestaurantForm";
+import styles from "@src/pages/AddRestaurantPage/AddRestaurantPage.module.scss";
+
+const AddRestaurantPage = () => {
+  return (
+    <div>
+      <Header />
+      <div className={styles.RectOnImg}>
+        <span className={styles.TitleSearch}>My new restaurant</span>
+      </div>
+      <Layout>
+        <RestaurantForm add />
+      </Layout>
+    </div>
+  );
+};
+
+export default AddRestaurantPage;
---
+++ packages/restoWeb/src/services/dishCalls.ts
@@ -0,0 +1,67 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:8081/api/dishes/";
+
+export const getAllDishes = async () => {
+  try {
+    const response = await axios({
+      method: "GET",
+      url: baseUrl,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching all dishes:", error);
+    throw new Error("Failed to fetch all dishes");
+  }
+};
+
+export const addNewDish = async (restoName: string, body: any) => {
+  try {
+    const response = await axios({
+      url: baseUrl + restoName,
+      method: "POST",
+      data: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error adding dish:", error);
+    throw new Error("Failed to add dish");
+  }
+};
+
+export const editDish = async (restoName: string, body: any) => {
+  try {
+    const response = await axios({
+      url: baseUrl + restoName,
+      method: "PUT",
+      data: JSON.stringify(body),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error editing dish:", error);
+    throw new Error("Failed to edit dish");
+  }
+};
+
+export const deleteDish = async (restoName: string, dishName: string) => {
+  try {
+    const response = await axios({
+      url: baseUrl + restoName,
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify({ name: dishName }),
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting dish:", error);
+    throw new Error("Failed to delete dish");
+  }
+};
