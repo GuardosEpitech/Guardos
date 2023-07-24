81524061fd9b82cafb0b34b6fd63d46461e40e6e
Ramon W <71701275+wernerramon@users.noreply.github.com>
2023-07-24 20:08:13 +0200

    [BE] [MODIFY] Eip 196 be test for product screen (#14)
    * [BE] [WIP] products test
    
    * [BE] [ADD] Test for products and coverage
    
    * [BE] [MODIFY] fixed merge error
    
    * [BE] [MODIFY] fixed missing coverage folder
    
    * [BE] [REMOVE] lint errors

--- packages/backend/cypress.config.ts
+++ packages/backend/cypress.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      require('@cypress/code-coverage/task')(on, config)
-      return config
+      require('@cypress/code-coverage/task')(on, config);/* eslint-disable-line */
+      return config;
       // implement node event listeners here
     },
   },
