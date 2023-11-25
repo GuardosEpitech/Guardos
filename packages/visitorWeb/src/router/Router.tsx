9ede81025007fa66a2be3b10a16dc4911f3fb81e
MarcPIster <71755408+MarcPIster@users.noreply.github.com>
2023-11-25 13:10:30 +0100

    Eip 199 fix allergens (#29)
    * [BE] [FIX] fix visitor chrash
    
    * [BE] [FIX] fix visitor chrash and refix

--- packages/visitorWeb/src/router/Router.tsx
+++ packages/visitorWeb/src/router/Router.tsx
@@ -5,13 +5,11 @@ import HomePage from "@src/pages/HomePage";
 import RegistrationPage from "@src/pages/RegistrationPage";
 import LoginPage from "@src/pages/LoginPage";
 import MapPage from "@src/pages/MapPage";
-import ScrollToTop from "shared/components/ScrollToTop/ScrollToTop";
 import ContactPage from "@src/pages/ContactPage/ContactPage";
 
 const MVPRouter = () => {
   return (
     <BrowserRouter>
-      <ScrollToTop />
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegistrationPage />} />
