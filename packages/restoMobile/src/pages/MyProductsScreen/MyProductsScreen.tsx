a3d7e7bf8836c43b02a7fab1870ef3954602e7a4
JosefineM2000 <josefine.mende@epitech.eu>
2023-11-26 09:51:20 +0700

    [FE] [ADD] rest app navigation

---
+++ packages/restoMobile/src/pages/MyProductsScreen/MyProductsScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MyProductsScreen: React.FC = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>My Products Screen</Text>
+      {/* Add your content or functionality related to My Dishes here */}
+    </View>
+  );
+};
+
+export default MyProductsScreen;
---
+++ packages/restoMobile/src/pages/MyRestaurantsScreen/MyRestaurantsScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MyRestaurantsScreen: React.FC = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>My Restaurants Screen</Text>
+      {/* Add your content or functionality related to My Dishes here */}
+    </View>
+  );
+};
+
+export default MyRestaurantsScreen;
