ff21857f55be552b212514ffdad00ff5f48ea787
MarcPIster <71755408+MarcPIster@users.noreply.github.com>
2023-12-13 20:10:04 +0100

    [BE] [MERGE] delete/post change functionality for images  (#35)
    * [FE] [FIX] fix error with allergens not saving
    
    * [BE] [ADD] added backend functionality for uploading images
    
    * Eip 201 picture storage with tests (#38)
    
    * [BE] [WIP] work on uploading images
    
    * [BE] [WIP] work on uploading images, remove old stuff
    
    * [BE] [WIP] work on uploading images, basic upload is working somehow xD
    
    * [BE] [ADD] upload and saving images in backend
    
    * [BE] [WIP] see todos --> implement images in frontend, send them to backend, add image id instade of url
    
    * [BE] [FIX] add types, without types, backend will not start
    
    * [BE] [ADD] add functionality for uploading pictures for: restaurant, extras and dishes
    
    * [BE] [ADD] add tests for images and better errorhandling
    
    * [BE] [MERGE] handle images in backend (#40)
    
    * [BE] [WIP] work on uploading images
    
    * [BE] [WIP] work on uploading images, remove old stuff
    
    * [BE] [WIP] work on uploading images, basic upload is working somehow xD
    
    * [BE] [ADD] upload and saving images in backend
    
    * [BE] [WIP] see todos --> implement images in frontend, send them to backend, add image id instade of url
    
    * [BE] [FIX] add types, without types, backend will not start
    
    * [BE] [ADD] add functionality for uploading pictures for: restaurant, extras and dishes
    
    * [BE] [ADD] add tests for images and better errorhandling
    
    * [BE] [ADD] add functionality and route for deleting images
    
    * [BE] [FIX] fix errorhandling
    
    * [BE] [ADD] add more tests for images
    
    * [BE] [ADD] add even more tests for images
    
    * [BE] [ADD] add functionality to get image/images
    
    * [BE] [ADD] add functionality to change image via id
    
    * [BE] [ADD] add tests for changing and deleting and change error handling

--- packages/backend/src/models/imageInterfaces.ts
+++ packages/backend/src/models/imageInterfaces.ts
@@ -9,5 +9,14 @@ const imageSchema = new mongoose.Schema({
   base64: String,
 });
 
+export interface IImage {
+    _id: number;
+    filename: string;
+    contentType: string;
+    size: number;
+    uploadDate: Date;
+    base64: string;
+}
+
 const Image = mongoose.model('Image', imageSchema);
 export default Image;
