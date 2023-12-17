f972631d9c012ead682ea19d958eebc33fd05f47
MarcPIster <marc.pister@epitech.eu>
2023-12-17 10:35:19 +0100

    [BE] [fix] fix backend--> starts now

--- packages/backend/src/routes/logout.ts
+++ packages/backend/src/routes/logout.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import { Response, Request } from 'express';
-import { logoutUserResto } from '../controllers/userRestoController';
+//import { logoutUserResto } from '../controllers/userRestoController';
 
 const router = express.Router();
 
@@ -15,19 +15,19 @@ router.post('/', async function (req: Request, res: Response) {
   }
 });
 
-router.post('/restoWeb', async function (req: Request, res: Response) {
-  try {
-    const data = req.body;
-    const answer = await logoutUserResto(data.token);
-
-    if (answer) {
-      return res.status(200).send(answer);
-    }
-    return res.status(404).send('Could not find the User to logout');
-  } catch (error) {
-    return res.status(500)
-      .send('An error occurred while processing your request');
-  }
-});
+//router.post('/restoWeb', async function (req: Request, res: Response) {
+//  try {
+//    const data = req.body;
+//    const answer = await logoutUserResto(data.token);
+//
+//    if (answer) {
+//      return res.status(200).send(answer);
+//    }
+//    return res.status(404).send('Could not find the User to logout');
+//  } catch (error) {
+//    return res.status(500)
+//      .send('An error occurred while processing your request');
+//  }
+//});
 
 export default router;
