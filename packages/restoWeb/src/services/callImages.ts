1e7379bbf94a3fd4783825ab98d5c450005303a1
MarcPIster <marc.pister@epitech.eu>
2024-02-20 18:01:55 +0100

    [FE] [ADD] add picture managment restaurant web for dishes and restaurants

--- packages/restoWeb/src/services/callImages.ts
+++ packages/restoWeb/src/services/callImages.ts
@@ -33,7 +33,7 @@ export const getImages = async (imageIds: number[]) => {
   }
 };
 
-export const addImage = async (restaurantName: string, 
+export const addImageResto = async (restaurantName: string,
   imageName: string, contentType: string, size: number, base64: string) => {
   try {
     const body = {
@@ -79,3 +79,51 @@ export const deleteImageRestaurant =
       }
     };
 
+export const addImageDish = async (restaurantName: string, dishName: string,
+  imageName: string, contentType: string, size: number, base64: string) => {
+  try {
+    const body = {
+      restaurant: restaurantName,
+      dish: dishName,
+      image: {
+        filename: imageName,
+        contentType: contentType,
+        size: size,
+        base64: base64,
+      },
+    };
+    const response = await axios({
+      url: baseUrl,
+      method: "POST",
+      data: body,
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    return response.data;
+  }
+  catch (error) {
+    console.error("Error adding image:", error);
+    throw new Error("Failed to add image");
+  }
+};
+
+export const deleteImageDish = async (imageId: number, restaurantName: string,
+  dishName: string) => {
+  try {
+    const body = {
+      restaurant: restaurantName,
+      dish: dishName,
+      imageId: imageId
+    };
+    const response = await axios({
+      url: baseUrl,
+      method: "DELETE",
+      data: body,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting image:", error);
+    throw new Error("Failed to delete image");
+  }
+};
