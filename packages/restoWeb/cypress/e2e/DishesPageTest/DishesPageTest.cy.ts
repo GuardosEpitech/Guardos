fca0b61bdf972a18be5d5e560149096d8372c430
MarcPIster <marc.pister@epitech.eu>
2024-02-26 16:03:54 +0100

    [FE] [FIX] fix login error

--- packages/restoWeb/cypress/e2e/DishesPageTest/DishesPageTest.cy.ts
+++ packages/restoWeb/cypress/e2e/DishesPageTest/DishesPageTest.cy.ts
@@ -1,15 +1,4 @@
-const login = (email:string) => {
-    cy.session(email, () => {
-        cy.viewport(1710, 948);
-        cy.visit('http://localhost:8080/login');
-        cy.wait(100);
-        cy.get('#\\:r0\\:').type(email);
-        cy.wait(100);
-        cy.get('#\\:r1\\:').type('TestTest1');
-        cy.get('.MuiButtonBase-root').click();
-        cy.wait(3000);
-    })
-}
+import {login} from '../../fixtures/login';
 
 describe('DishesPageTest check dish card', () => {
     it('passes', () => {
