016d5d8402aa1a83a439c4994874be63c4b4ebc1
JosefineM2000 <josefine.mende@epitech.eu>
2024-03-12 20:17:19 +0700

    [FE] [ADD] favourite lists - dish and resto

--- packages/visitorWeb/src/pages/MenuPage/MenuPage.tsx
+++ packages/visitorWeb/src/pages/MenuPage/MenuPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { useLocation } from "react-router-dom";
 import styles from "@src/pages/MenuPage/MenuPage.module.scss";
 import Dish from "@src/components/menu/Dish/Dish";
@@ -9,6 +9,7 @@ import { List, ListItem } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { ICategories } from "shared/models/categoryInterfaces";
 import { IDishFE } from "shared/models/dishInterfaces";
+import {getDishFavourites} from "@src/services/favourites";
 
 const theme = createTheme({
   palette: {
@@ -19,7 +20,24 @@ const theme = createTheme({
 });
 
 const MenuPage = () => {
-  const { menu, restoName, address } = useLocation().state;
+  const { menu, restoName, restoID, address } = useLocation().state;
+  const [isFavouriteDishs, setIsFavouriteDishs] = React.useState<Array<{ restoID: number, dish: IDishFE }>>([]);
+
+  useEffect(() => {
+    fetchFavourites().then(r => console.log("Loaded favourite dish list"));
+  }, [])
+
+  const fetchFavourites = async () => {
+    const userToken = localStorage.getItem('user');
+    if (userToken === null) { return; }
+
+    try {
+      const favouriteDishIds = await getDishFavourites(userToken);
+      setIsFavouriteDishs(favouriteDishIds);
+    } catch (error) {
+      console.error("Error fetching user favourites:", error);
+    }
+  };
 
   return (
     <>
@@ -45,6 +63,9 @@ const MenuPage = () => {
               {category.dishes.length > 0 &&
                 <Category key={category.name + index} title={category.name}>
                   {category.dishes.map((dish: IDishFE, index: number) => {
+                    const isFavourite = isFavouriteDishs.some(fav => {
+                      return fav.restoID === restoID && fav.dish.uid === dish.uid;
+                    });
                     return (
                       <Dish
                         key={dish.name + index}
@@ -54,6 +75,9 @@ const MenuPage = () => {
                         options={dish.category.extraGroup.join(", ")}
                         price={dish.price}
                         picturesId={dish.picturesId}
+                        restoID={restoID}
+                        dishID={dish.uid}
+                        isFavourite={isFavourite}
                       />
                     )
                   })}
