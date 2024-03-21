fd4e5ab2bc9126c9d1c6dcdeb4e1e4c1260ca4ac
Josi <71701074+JosefineM2000@users.noreply.github.com>
2024-03-21 23:35:00 +0700

    [BE] [MODIFY] add try catch for all routes (#74)

--- packages/backend/src/routes/foodCategories.ts
+++ packages/backend/src/routes/foodCategories.ts
@@ -10,14 +10,20 @@ import { checkIfRestaurantExistsWithId } from '../middleware/restaurantMiddleWar
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  if (!await checkIfRestaurantExistsWithId(req.body.restaurantId)) {
-    return res.status(400)
-      .send('No Restaurant with the provided Id was found');
+  try {
+    if (!await checkIfRestaurantExistsWithId(req.body.restaurantId)) {
+      return res.status(400)
+        .send('No Restaurant with the provided Id was found');
+    }
+    const foodCategories = await getFoodCategoriesBasedOnRestaurant(req.body.restaurantId);
+
+    return res.status(200)
+      .send(foodCategories);
+  } catch (error) {
+    console.error("Error in 'foodCategorie' route:", error);
+    return res.status(500)
+      .send({ error: 'Internal Server Error' });
   }
-  const foodCategories = await getFoodCategoriesBasedOnRestaurant(req.body.restaurantId);
-  
-  return res.status(200)
-    .send(foodCategories);
 });
 
 router.put('/', async (req, res) => {
@@ -38,42 +44,53 @@ router.put('/', async (req, res) => {
         .send(foodCategorie);
     }
   } catch (e) {
-    console.error(e);
+    console.error("Error in 'foodCategorie' route:", e);
     return res.status(404)
       .send('Put Food Categorie failed');
   }
 });
 
 router.post('/', async (req, res) => {
-  if (!await checkIfRestaurantExistsWithId(req.body.restaurantId)) {
-    return res.status(400)
-      .send('No Restaurant with the provided Id was found');
-  }
-  if (req.body.name === '') {
-    return res.status(400)
-      .send('No Food Categorie Name was given');
-  }
-  await createFoodCategorie(req.body.restaurantId, req.body.name);
+  try {
+    if (!await checkIfRestaurantExistsWithId(req.body.restaurantId)) {
+      return res.status(400)
+        .send('No Restaurant with the provided Id was found');
+    }
+    if (req.body.name === '') {
+      return res.status(400)
+        .send('No Food Categorie Name was given');
+    }
+    await createFoodCategorie(req.body.restaurantId, req.body.name);
 
-  return res.status(200)
-    .send('Food Categorie '
-      + req.body.name + ' saved ' + ' with restaurant id ' + req.body.restaurantId);
-})
+    return res.status(200)
+      .send('Food Categorie ' + req.body.name + ' saved ' +
+        ' with restaurant id ' + req.body.restaurantId);
+  } catch (error) {
+    console.error("Error in 'foodCategorie' route:", error);
+    return res.status(500)
+      .send({ error: 'Internal Server Error' });
+  }
+});
 
 router.delete('/', async (req, res) => {
+  try {
+    if (!await checkIfRestaurantExistsWithId(req.body.restaurantId)) {
+      return res.status(400)
+        .send('No Restaurant with the provided Id was found');
+    }
 
-  if (!await checkIfRestaurantExistsWithId(req.body.restaurantId)) {
-    return res.status(400)
-      .send('No Restaurant with the provided Id was found');
-  }
-
-  const deleteStatus = await deleteFoodCategorieByID(req.body.foodCategorieID)
-  if (deleteStatus) {
-    return res.status(200)
-      .send('Food Categorie deleted with id ' + req.body.foodCategorieID);
-  } else {
-    return res.status(400)
-      .send('Food Categorie not found');
+    const deleteStatus = await deleteFoodCategorieByID(req.body.foodCategorieID);
+    if (deleteStatus) {
+      return res.status(200)
+        .send('Food Categorie deleted with id ' + req.body.foodCategorieID);
+    } else {
+      return res.status(400)
+        .send('Food Categorie not found');
+    }
+  } catch (error) {
+    console.error("Error in 'foodCategorie' route:", error);
+    return res.status(500)
+      .send({ error: 'Internal Server Error' });
   }
 });
 
