4d1151507d91d9ac89055a7b023601b57af93faf
Alban de Tourtier <alban.detourtier@bring4you.com>
2024-03-21 18:21:01 +0000

    [MERGE] Conflict resolved

--- packages/visitorWeb/src/components/RestoCard/RestoCard.tsx
+++ packages/visitorWeb/src/components/RestoCard/RestoCard.tsx
@@ -117,7 +117,7 @@ const RestoCard = (props: IRestoCardProps) => {
         <Grid item xs={9} className={styles.GridItem}>
           <div className={styles.FlexParent}>
             <h3 className={styles.DishTitle}>{name}</h3>
-            <Rating restoRating={rating} restoRatingsCount={ratingCount} />
+            <Rating restoRating={rating} restoRatingsCount={ratingCount} restoName={name} />
             <div className={styles.FavoriteIcon} onClick={handleFavoriteClick}>
               {isFavorite ? (
                 <FavoriteIcon id="favourite" color="error" />
@@ -125,6 +125,7 @@ const RestoCard = (props: IRestoCardProps) => {
                 <FavoriteBorderIcon id="no-favourite" color="error" />
               )}
             </div>
+
           </div>
           <div className={styles.FlexParent}>
             <PlaceIcon />
