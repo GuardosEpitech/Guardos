431378f46630859e3746c8ad29743620141dc039
Dubois Renan <renan.dubois@epitech.eu>
2024-03-22 13:05:46 +0900

    [FE] [FIX] Move the request feature button for the VisitorWeb and remove the login the back

--- packages/backend/src/routes/featureRequest.ts
+++ packages/backend/src/routes/featureRequest.ts
@@ -1,17 +1,13 @@
 import express from 'express';
 import * as nodemailer from 'nodemailer';
 import { Response, Request } from 'express';
-
-
 import 'dotenv/config';
-import { log } from 'console';
 
 const router = express.Router();
 
 router.post('/', async function (req: Request, res: Response) {
   try {
     const data = req.body
-    log(data)
     const smtpConfig = {
       host: 'smtp.office365.com',
       port: 587,
@@ -25,11 +21,11 @@ router.post('/', async function (req: Request, res: Response) {
     const transporter = nodemailer.createTransport(smtpConfig);
 
   
-    const mailOptions = {
+    const mailOptions: nodemailer.SendMailOptions = {
       from: data.name,
       to: 'guardos-help@outlook.com',
       subject: data.subject,
-      text: data.request
+      text: `Name: ${data.name}\nRequest:${data.request}`,
     };
   
     await transporter.sendMail(mailOptions);
--- packages/visitorWeb/src/pages/MyAccountPage/MyAccountPage.tsx
+++ packages/visitorWeb/src/pages/MyAccountPage/MyAccountPage.tsx
@@ -5,6 +5,7 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
+import {Button,Typography} from '@mui/material';
 
 import styles from "./MyAccountPage.module.scss";
 import {deleteAccount} from "@src/services/userCalls";
@@ -13,7 +14,6 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import Button from '@mui/material/Button';
 import {changeVisitorPassword, editVisitorProfileDetails, getVisitorProfileDetails} from "@src/services/profileCalls";
 import TextField from "@mui/material/TextField";
 
@@ -335,7 +335,13 @@ const MyAccountPage = () => {
           </button>
         </div>
         <button className={styles.deleteButton} onClick={handleOpenDeletePopup}>Delete Account</button>
-      </div>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
+          <Typography variant="body1">You need a new feature? </Typography>
+          <Button onClick={() => window.location.href = '/feature-request'}>
+          Just ask for it !
+          </Button>
+      </div>   
+      </div>         
       <div className={styles.restaurantSection}>
         <h1>Last Watched Restaurants</h1>
         <ul>
