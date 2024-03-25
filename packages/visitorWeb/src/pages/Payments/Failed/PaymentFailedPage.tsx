c811a4406b433bb3e5eb10abb275ec5963d053bb
MarcPIster <marc.pister@epitech.eu>
2024-03-25 09:41:17 +0100

    [BE] [MERGE] resolve merge

--- packages/visitorWeb/src/pages/Payments/Failed/PaymentFailedPage.tsx
+++ packages/visitorWeb/src/pages/Payments/Failed/PaymentFailedPage.tsx
@@ -5,7 +5,8 @@ const PaymentFailedPage = () => {
   return (
       <div className={styles.paymentFailedContainer}>
         <h2>Payment Failed</h2>
-        <p>We're sorry, but your transaction could not be completed at this time. There may have been an issue with your payment method or the transaction was declined.</p>
+        <p>pull
+          We're sorry, but your transaction could not be completed at this time. There may have been an issue with your payment method or the transaction was declined.</p>
         <p>Please try again or contact us for assistance.</p>
         <a href="/payment" className={styles.tryAgainLink}>Try Again</a>
       </div>
