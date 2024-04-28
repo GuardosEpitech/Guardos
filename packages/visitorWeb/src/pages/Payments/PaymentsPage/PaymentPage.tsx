d343d443abb84a5dc583ac829b76fbd59699fd23
Ramon W <werner.ramon1@googlemail.com>
2024-04-29 03:05:53 +0900

    [FE] [ADD] payment page

---
+++ packages/visitorWeb/src/pages/Payments/PaymentsPage/PaymentPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './PaymentPage.module.scss';
+import {useTranslation} from 'react-i18next';
+
+const PaymentPage = () => {
+    const {t} = useTranslation();
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PaymentPage;
