40170621428b3c572ab9289e2caf0c9c04adde54
Ramon W <werner.ramon1@googlemail.com>
2024-05-14 12:15:42 +0900

    [FE] [MODIFY] updated payment visitor test

--- packages/visitorWeb/cypress/e2e/PaymentTest/PaymentTest.cy.ts
+++ packages/visitorWeb/cypress/e2e/PaymentTest/PaymentTest.cy.ts
@@ -11,6 +11,8 @@ describe('add payment method', function() {
         cy.visit('http://localhost:8082/payment');
         cy.wait(5000);
         cy.get('.V3JgFjoBAN8BwxEGAGJx > .cCywpriAWfC5rZb8oRYj > .h9M1OnE2sJy7LyKB0UIC > .O2clrE0PvzQaJu1FiVwb > button').click();
+        cy.wait(1000);
+        cy.url().should('not.eq', 'http://localhost:8082/payment');
               
     });
 });
@@ -26,8 +28,8 @@ describe('delete payment method', function() {
         cy.get('.MuiButtonBase-root').click();
         cy.wait(3000);
         cy.visit('http://localhost:8082/payment');
-        cy.get('.WZTdaearlP_yTUm6rMiL > .M_HDA_rG5PTFNqvWY5wT:nth-child(1) > .cqV5qvw4eqpyP90Jv1ii > div > .MuiSvgIcon-root').click();
-        cy.get('.M_HDA_rG5PTFNqvWY5wT > .FMASqhSItwfzxzFowxDP > .R1GAGLJdrbZcjXRGmAmM > div > button:nth-child(1)').click();
+        cy.get('.WZTdaearlP_yTUm6rMiL > .ny8NJaUaBgSyfjhrHKru:nth-child(1) > .cY7qFBw2qiflpVVIzLkU > div > .MuiSvgIcon-root').click();
+        cy.get('.WZTdaearlP_yTUm6rMiL > .ny8NJaUaBgSyfjhrHKru:nth-child(1) > .C0YdoCDQv7CxthbMYOF1 > .uzTovQkXEAfJKWhjv_4r > div > button:nth-child(1)').click();
     });
 });
        
