2ce39b7996a29cb888add6686f7397b0eb6bb482
Ramon W <ramon.werner@epitech.eu>
2024-06-18 18:28:18 +0900

    [BE] [ADD] dish discount test

---
+++ packages/backend/cypress/e2e/dishDiscount.cy.ts
@@ -0,0 +1,49 @@
+describe('adds and removes discount from dish', () => {
+    const dataAdd = {
+        "restoID": 52,
+        "dish": {
+            "name": "Coke",
+            "uid": 1,
+            "discount": 5
+
+        }
+    };
+    const dataRemove = {
+        "restoID": 52,
+        "dish": {
+            "name": "Coke",
+            "uid": 1
+        }
+    };
+
+    it('should add a discount to the dish Coke', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:8081/api/dishes/addDiscount',
+            body: dataAdd,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body).to.be.an('Object');
+            expect(response.body.discount).to.eq(5);
+        });
+    });
+    it('should remove discount from the dish Coke', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:8081/api/dishes/removeDiscount',
+            body: dataRemove,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body).to.be.an('Object');
+            expect(response.body.discount).to.eq(-1);
+        });
+    });
+});
