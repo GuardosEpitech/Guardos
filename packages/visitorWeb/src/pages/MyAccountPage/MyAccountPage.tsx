7e304b1b406b88cb8824b8cb470f9e1e46ed8686
Josi <71701074+JosefineM2000@users.noreply.github.com>
2024-10-19 15:31:52 +0200

    [FE] [FIX] sync allergens in profile and filter (#170)

--- packages/visitorWeb/src/pages/MyAccountPage/MyAccountPage.tsx
+++ packages/visitorWeb/src/pages/MyAccountPage/MyAccountPage.tsx
@@ -71,6 +71,23 @@ const MyAccountPage = () => {
 
   const [openAddIngredientPopup, setOpenAddIngredientPopup] = useState(false);
   const [newIngredient, setNewIngredient] = useState('');
+  // TODO: apply i18n
+  const allAllergens = [
+    'celery',
+    'gluten',
+    'crustaceans',
+    'eggs',
+    'fish',
+    'lupin',
+    'milk',
+    'molluscs',
+    'mustard',
+    'peanuts',
+    'sesame',
+    'soybeans',
+    'sulphides',
+    'tree nuts',
+  ];
 
 
   useEffect(() => {
@@ -529,8 +546,7 @@ const MyAccountPage = () => {
             label={t('pages.MyAccountPage.allergens')}
           >
             {
-              // TODO: apply i18n
-              ['peanut', 'gluten', 'dairy'].map((allergen) => (
+              allAllergens.map((allergen) => (
               <MenuItem key={allergen} value={allergen} selected={selectedOptions.includes(allergen)}>
                 {allergen}
               </MenuItem>
