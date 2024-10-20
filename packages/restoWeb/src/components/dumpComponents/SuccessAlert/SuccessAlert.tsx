cc890b13f1a33ab041decda9ea1cacd6d2d9df0b
Gylian Karsch <karschbuisness@web.de>
2024-10-20 17:31:10 +0200

    [FE] [ADD] Better Success Messages for the User

--- packages/restoWeb/src/components/dumpComponents/SuccessAlert/SuccessAlert.tsx
+++ packages/restoWeb/src/components/dumpComponents/SuccessAlert/SuccessAlert.tsx
@@ -4,8 +4,9 @@ import { useLocation } from "react-router-dom";
 import { Alert, Snackbar } from "@mui/material";
 import {useTranslation} from "react-i18next";
 
-const HomePage = () => {
+const HomePage = (props:any) => {
   const [hidden, setHidden] = React.useState(true);
+  const objectName = props.objectName;
   let successfulForm = useLocation().state?.successfulForm;
   const {t} = useTranslation();
 
@@ -29,7 +30,7 @@ const HomePage = () => {
   return (
     <Snackbar open={!hidden} autoHideDuration={6000} onClose={handleClose}>
       <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
-        {t('components.SuccessAlert.success-msg')}
+        {t('components.SuccessAlert.success-msg', {objectName: objectName})}
       </Alert>
     </Snackbar>
   );
