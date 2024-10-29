9d58d8175e8826909fd5482794637759652dcb7d
MarcPIster <marc.pister@epitech.eu>
2024-10-29 13:31:51 +0100

    [BE] [FIX] fix facebook login (automaticly verify mail..)

--- packages/backend/src/middleware/facebookAuthenticate.ts
+++ packages/backend/src/middleware/facebookAuthenticate.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Request, Response, NextFunction } from 'express';
+import {NextFunction, Request, Response} from 'express';
 
 export async function authenticateFacebook(
   req: Request, res: Response, next: NextFunction) {
@@ -7,20 +7,25 @@ export async function authenticateFacebook(
   const clientId = process.env.FACEBOOK_APP_ID;
   const clientSecret = process.env.FACEBOOK_APP_SECRET;
   const redirectUri = process.env.FACEBOOK_REDIRECT_URI;
+  let url = process.env.allowedVW;
+  if (url.endsWith(':')) {
+    url = `${process.env.allowedVW}${process.env.PORTVW}`;
+  }
 
   if (!code) {
-    return res.status(400)
-      .send('No code provided');
+    return res.redirect(`${url}/login?error=auth_failed`);
   }
 
   try {
     const tokenResponse = await axios.get(`https://graph.facebook.com/v10.0/oauth/access_token?client_id=${clientId}&redirect_uri=${redirectUri}&client_secret=${clientSecret}&code=${code}`);
-    const accessToken = tokenResponse.data.access_token;
-    req.accessToken = accessToken;
+    req.accessToken = tokenResponse.data.access_token;
     next();
   } catch (error) {
+    let url = process.env.allowedVW;
+    if (url.endsWith(':')) {
+      url = `${process.env.allowedVW}${process.env.PORTVW}`;
+    }
     console.error('Error during Facebook authentication:', error);
-    res.status(500)
-      .send('Facebook authentication failed');
+    return res.redirect(`${url}/login?error=auth_failed`);
   }
 }
