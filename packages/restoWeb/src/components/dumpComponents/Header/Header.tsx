42646302ab9f6a5e01519f8e96a952eb9834e83e
Neotehn <71703332+Neotehn@users.noreply.github.com>
2024-11-12 10:52:16 +0100

    [FE] [MODIFY] added missing headerlink (#195)

--- packages/restoWeb/src/components/dumpComponents/Header/Header.tsx
+++ packages/restoWeb/src/components/dumpComponents/Header/Header.tsx
@@ -101,6 +101,7 @@ const Header = () => {
                 <a className={styles.NavTitleDropDown} onClick={logoutUser} role='link' tabIndex={0} href="javascript:void(0);">{t('components.Header.logout')}</a>
                 <a className={styles.NavTitleDropDown} href="/account" onClick={() => { handleOptionClick(); navigate('/account'); }}>{t('components.Header.my-account')}</a>
                 <a className={styles.NavTitleDropDown} href="/restoOverview" onClick={() => { handleOptionClick(); navigate('/restoOverview'); }}>{t('common.resto-overview')}</a>
+                <a className={styles.NavTitleDropDown} href="/insights" onClick={() => { handleOptionClick(); navigate('/insights'); }}>{t('common.insights')}</a>
                 <a className={styles.NavTitleDropDown} href="/guides" onClick={() => { handleOptionClick(); navigate('/guides'); }}>{t('components.Header.guides')}</a>
               </>
             )}
