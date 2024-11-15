ec6686fcbb5e0f2432164eac6fa766a165d06373
Ramon W <ramon.werner@epitech.eu>
2024-11-15 18:56:33 +0100

    [FE] [FIX] fix double click rating

--- packages/visitorWeb/src/pages/RestoPage/RestoPage.tsx
+++ packages/visitorWeb/src/pages/RestoPage/RestoPage.tsx
@@ -237,7 +237,7 @@ const RestoPage = () => {
 
     const newFilter = {
       range: filter.range,
-      rating: [rating, 5],
+      rating: [filter.rating[0], 5],
       name: inputFields[0],
       location: inputFields[1],
       categories: categories.filter(category =>
